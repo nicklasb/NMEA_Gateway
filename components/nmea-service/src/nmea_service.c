ad7c6553efea0bb0fe8edb1cfda0634e019b2377
Nicklas Börjesson <nicklasb@gmail.com>
2023-09-21 01:07:46 +0200

    Add set_heading function

--- components/nmea-service/src/nmea_service.c
+++ components/nmea-service/src/nmea_service.c
@@ -14,6 +14,7 @@
 const uint16_t serviceid = 1959;
 
 #define SPEED_THROUGH_WATER_PGN 128259L
+#define SET_EVO_PILOT_COURSE 126208L
 
 void on_incoming(robusto_message_t *message);
 void shutdown_nmea_network_service(void) ;
@@ -39,12 +40,20 @@ volatile network_service_t nmea_service = {
 void on_incoming(robusto_message_t *message) {
     
     if (message->binary_data_length > 0) {
+        int32_t curr_value = *(uint32_t *)(message->binary_data);
         rob_log_bit_mesh(ROB_LOG_INFO, nmea_log_prefix, message->binary_data, message->binary_data_length);
-        if (*(uint32_t *)(message->binary_data) == SPEED_THROUGH_WATER_PGN) {
-            double speed_through_water =  (double)(*(uint16_t *)(message->binary_data + sizeof(uint32_t)))/1000;
+        if (curr_value == SPEED_THROUGH_WATER_PGN) {
+            double speed_through_water =  (double)(*(uint16_t *)(message->binary_data + sizeof(uint32_t)))/100;
             NMEA2000_Controller_send_speedThroughWater(speed_through_water);
             ROB_LOGI(nmea_log_prefix, "Sent speed from %s: %f knots!", message->peer->name, speed_through_water);
+        }
+        else if (curr_value == SET_EVO_PILOT_COURSE) {
             
+            float curr_heading = (float)(*(float *)(message->binary_data + 4));
+            int change =  (int)(*(int *)(message->binary_data + 8));
+            NMEA2000_Controller_set_heading(curr_heading, change);
+            ROB_LOGI(nmea_log_prefix, "Sent heading change from %s: Heading - %f, Change %i  knots!", message->peer->name, curr_heading,change);
+        
         } else {
             ROB_LOGE(nmea_log_prefix, "An unrecognized PGN: %lu", *(uint32_t *)(message->binary_data));
         }
