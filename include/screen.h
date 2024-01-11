537d113e37d931dff134b87a456f9c7c203daeba
Nicklas Börjesson <nicklasb@gmail.com>
2024-01-11 22:13:40 +0100

    Move to LVGL

--- include/screen.h
+++ include/screen.h
@@ -1,6 +1,15 @@
-
 #pragma once
+
 #include <robconfig.h>
+#include <stdint.h>
+#include <lvgl.h>
+
+lv_obj_t * get_current_screen();
+
+void label_set_text(lv_obj_t * label, const char * txt);
 
 
+void robusto_screen_minimal_write_xy(char * txt, uint16_t x, uint16_t y, uint8_t height);
+void robusto_screen_minimal_write(char * txt, uint8_t col, uint8_t row);
+void robusto_screen_minimal_write_small(char * txt, uint8_t col, uint8_t row);
 void init_screen(char * _log_prefix);
--- main/screen.c
+++ main/screen.c
@@ -1,10 +1,45 @@
 #include "screen.h"
 #include <robusto_screen.h>
-static char * ui_log_prefix;
+#include <lvgl.h>
+#include <robusto_logging.h>
+
+static char *screen_log_prefix;
+static lv_disp_t *display;
+static lv_obj_t *screen;
+
+lv_obj_t *get_current_screen()
+{
+    return screen;
+}
+
+void robusto_screen_minimal_write_xy(char *txt, uint16_t x, uint16_t y, uint8_t height)
+{
+}
+void robusto_screen_minimal_write(char *txt, uint8_t col, uint8_t row)
+{
+    robusto_screen_minimal_write_xy(txt, col * 8, row * 16, 16);
+}
+
+void robusto_screen_minimal_write_small(char *txt, uint8_t col, uint8_t row)
+{
+    robusto_screen_minimal_write_xy(txt, col * 8, row * 12, 12);
+}
+
+void label_set_text(lv_obj_t * label, const char * txt) {
+    if (robusto_screen_lvgl_port_lock(2000)) {
+        lv_label_set_text(label, txt);
+        robusto_screen_lvgl_port_unlock();
+    } else {
+        ROB_LOGE(screen_log_prefix, "Failed to get a lock on the LVGL port in 2000 ms");
+    }
+}
 
 
-
-void init_screen(char * _log_prefix) {
-    ui_log_prefix = _log_prefix;
-    robusto_screen_init(ui_log_prefix);
+void init_screen(char *_log_prefix)
+{
+    screen_log_prefix = _log_prefix;
+    robusto_screen_init(screen_log_prefix);
+    
+    display = robusto_screen_lvgl_get_active_display();
+    screen = lv_disp_get_scr_act(display);
 }
