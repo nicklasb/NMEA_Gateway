0d5894168065c4f034d88f5ee9c56a8c54443736
Nicklas Börjesson <nicklasb@gmail.com>
2024-06-20 21:44:10 +0200

    Update changes

--- main/main.c
+++ main/main.c
@@ -11,9 +11,6 @@
 
 char *log_prefix;
 
-#define CAN_RX_IDX CONFIG_CAN_RX_IDX
-#define CAN_TX_IDX CONFIG_CAN_TX_IDX
-
 void app_main()
 {
     #ifndef CONFIG_ROBUSTO_PUBSUB_SERVER
@@ -23,7 +20,7 @@ void app_main()
     #ifdef CONFIG_ROBUSTO_UI
     set_cb_nmea_service(&set_server_stats, &set_nmea_stats);
     #endif
-    register_nmea_service();
+  //  register_nmea_service();
 
     init_robusto();    
 
@@ -36,10 +33,10 @@ void app_main()
 
     // INIT NMEA
     ROB_LOGI(log_prefix, "Starting NMEA2000 interface...");
-    NMEA2000_Controller_setup();
+    //NMEA2000_Controller_setup();
     char * taskname;
     asprintf(&taskname, "Looking for pilot");
-    robusto_create_task(&look_for_pilot, NULL, taskname , NULL, 0);
+  //  robusto_create_task(&look_for_pilot, NULL, taskname , NULL, 0);
 // robusto_peer_t *peer = add_peer_by_mac_address("Consumer", kconfig_mac_to_6_bytes(0x08b61fc0d660), ROBUSTO_MT_ESPNOW);
 // robusto_peer_t *peer = add_peer_by_i2c_address("Consumer", 1);
 
@@ -52,7 +49,7 @@ void app_main()
     ROB_LOGI(log_prefix, "------------------------------------------");
     while (1)
     {
-        NMEA2000_loop();
+      //  NMEA2000_loop();
         // ESP_LOGI(TAG, "Server available memory: %i", heap_caps_get_free_size(MALLOC_CAP_8BIT));
         robusto_yield();
     }
